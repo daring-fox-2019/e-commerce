3d6369eaac5d183dd283ea08dc301ad8257c9bc6
mhmmd-ysf <muhammad_yusuf22@outlook.co.id>
2019-05-14 00:24:56 +0700

    Added Test files

---
+++ server/test/router.test.js
@@ -0,0 +1,26 @@
+const chai = require('chai')
+const chaihttp = require('chai-http')
+const { expect } = chai
+const app = require('../app')
+const { Product } = require('../models')
+
+chai.use(chaihttp)
+
+describe('Error', (done) => {
+  it('Should return status 404 with object message \'Not Found :(\'', (done) => {
+    chai
+      .request(app)
+      .get('/items')
+      .then(res => {
+        expect(res).to.have.status(404)
+        expect(res.body).to.be.an('object')
+        expect(res.body).to.have.property('message')
+        expect(res.body.message).to.be.a('string')
+        expect(res.body.message).to.be.equal('Not Found :(')
+        done()
+      })
+      .catch(err => {
+        console.log({ err })
+      })
+  })
+})
