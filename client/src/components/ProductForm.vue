11ca6d9888e5ea809506f788a7b4894b0c2cf080
Andre Suchitra <89.andre@gmail.com>
2019-05-18 15:20:08 +0700

    add product stock qty & img component

--- client/src/components/ProductForm.vue
+++ client/src/components/ProductForm.vue
@@ -9,7 +9,12 @@
       </v-layout>
     </v-flex>
     <v-flex xs12 lg8>
-      <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="$emit('${type}', product)">
+      <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+        @submit.prevent="$emit('${type}', product)"
+      >
         <v-text-field v-model="product.title" :rules="titleRules" label="Title" required></v-text-field>
         <v-textarea
           box
@@ -17,41 +22,39 @@
           v-bind:value="product.description"
           placeholder="Enter your product description"
         ></v-textarea>
-
-        <v-select
-          v-model="select"
-          :items="items"
-          :rules="[v => !!v || 'Item is required']"
-          label="Item"
-          required
-        ></v-select>
-
-        <v-checkbox
-          v-model="checkbox"
-          :rules="[v => !!v || 'You must agree to continue!']"
-          label="Do you agree?"
-          required
-        ></v-checkbox>
-
-        <v-btn small :disabled="!valid" color="success" @click="validate">Validate</v-btn>
-
+        <v-flex py-3>
+            <h2 class="mb-2">Stock Quantity</h2>
+            <Quantity :quantity="product.stock" @updatequantity="updateProductStock"></Quantity>
+        </v-flex>
+        <v-flex py-3>
+            <h2>Product Image</h2>
+            <input class="mt-3" type="file" @change="onFileChange" />
+            <div id="preview">
+                <img v-if="imageurl" :src="imageurl" />
+            </div>
+        </v-flex>
         <v-btn small color="error" @click="reset">Reset Form</v-btn>
-
         <v-btn small color="warning" @click="resetValidation">Reset Validation</v-btn>
       </v-form>
     </v-flex>
   </v-layout>
 </template>
 <script>
+import Quantity from '@/components/Quantity.vue'
+
 export default {
   props: ["type"],
+  components: {
+      Quantity,
+  },
   data: () => ({
     valid: true,
+    imageurl: '',
     product: {
-        title: '',
-        description: '',
-        image: '',
-        stock: 0,
+      title: "",
+      description: "",
+      image: "",
+      stock: 0
     },
     titleRules: [v => !!v || "Title is required"],
     select: null,
@@ -59,6 +62,13 @@ export default {
     checkbox: false
   }),
   methods: {
+    updateProductStock(qty) {
+        this.product.stock = qty
+    },
+    onFileChange(e) {
+      const file = e.target.files[0];
+      this.imageurl = URL.createObjectURL(file);
+    },
     validate() {
       if (this.$refs.form.validate()) {
         this.snackbar = true;
@@ -66,6 +76,13 @@ export default {
     },
     reset() {
       this.$refs.form.reset();
+      this.product= 
+      { 
+        title: "",
+        description: "",
+        image: "",
+        stock: 0,
+      }
     },
     resetValidation() {
       this.$refs.form.resetValidation();
@@ -73,4 +90,18 @@ export default {
   }
 };
 </script>
+<style scoped>
+#preview {
+    margin-top: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+#preview img {
+  max-width: 100%;
+  max-height: 500px;
+}
+</style>
+
 
---
+++ client/src/components/Quantity.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="quantity-toggle">
+        <button @click="decrement">&mdash;</button>
+            <input style="min-width: 60px;" v-model="finalquantity" type="text"  @change="updateQty">
+        <button @click="increment">&#xff0b;</button>
+    </div>
+</template>
+<script>
+export default {
+  props: ['quantity'],
+  data () {
+    return {
+      internalquantity: 0,
+    }
+  },
+  watch: {
+      quantity: function(oldval, newval){
+        //   this.internalquantity = this.$props.quantity;
+      }
+  },
+  computed: {
+      finalquantity: {
+          get: function() {
+              return this.internalquantity;
+          },
+          set: function(value){
+              this.internalquantity = Number(value) !== NaN ? Number(value) : 0
+              console.log(this.internalquantity);
+          }
+      }
+  },
+  mounted() {
+      if(this.$props.quantity) {
+          this.internalquantity = this.$props.quantity >= 0 ? this.$props.quantity : 0
+      }
+  },
+  methods: {
+    increment () {
+        this.internalquantity++
+        this.$emit('updatequantity', this.internalquantity)
+    },
+    decrement (e) {
+        console.log('dec...', e);
+      if(this.internalquantity > 0) {
+        this.internalquantity--
+      }
+      this.$emit('updatequantity', this.internalquantity)
+    },
+    updateQty() {
+        this.$emit('updatequantity', this.finalquantity);
+    }
+  }
+}
+</script>
+<style scoped>
+
+.quantity-toggle {
+  display: flex;
+  border-radius: 4px;
+}
+.quantity-toggle input {
+    border: 0;
+    border-top: 2px solid #ddd;;
+    border-bottom: 2px solid #ddd;;
+    width: 2.5rem;
+    text-align: center;
+    padding: 0 .5rem;
+}
+.quantity-toggle button {
+    border: 2px solid #ddd;;
+    padding: .5rem;
+    background: #f5f5f5;
+    color: #888;
+    font-size: 1rem;
+    cursor: pointer;
+  }
+</style>
+
+
