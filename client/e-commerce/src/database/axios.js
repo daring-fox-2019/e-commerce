fb74b87ac2ad089e3d17b40caf226b7e86f39e9a
komangmahendra <komangwp.mahendra@gmail.com>
2019-05-20 07:47:33 +0700

    fix

--- client/e-commerce/src/database/axios.js
+++ client/e-commerce/src/database/axios.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
 
 const axiosInstance = Axios.create({
-  baseURL: 'http://server-ecommerce.komangmahendra.me',
+  //baseURL: 'http://server-ecommerce.komangmahendra.me',
+  baseURL: 'http://localhost:3000'
 });
 
 export default axiosInstance;
