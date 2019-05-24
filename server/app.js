603c53643265927926f70f399ccdaf1f19d1bba8
afdallismen <afdal.lismen@gmail.com>
2019-05-24 13:44:59 +0700

    setup deploy

--- server/app.js
+++ server/app.js
@@ -3,20 +3,23 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const multer = require('multer')
+const multerGoogleStorage = require('multer-google-storage')
 const cors = require('cors')
 
 const routes = require('./routes')
 
 const app = express()
 const PORT = process.env.PORT || 3000
-const uploads = process.env.NODE_ENV === 'test'
-  ? multer()
-  : multer({ dest: 'uploads/' })
+const uploads = multer({
+  storage: multerGoogleStorage.storageEngine()
+})
+// const uploads = process.env.NODE_ENV === 'test'
+//   ? multer()
+//   : multer({ dest: 'uploads/' })
 
-const DB = `mongodb://localhost:27017/ecommerce${'_' + process.env.NODE_ENV}`
-mongoose.connect(DB, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
 
-app.use(express.static(__dirname))
+// app.use(express.static(__dirname))
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
